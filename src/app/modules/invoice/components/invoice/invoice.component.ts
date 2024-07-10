eb233b27fd0aa296a3a2b31d5732cc0c0faca280
Nassime HARMACH <Nassitch@hotmail.fr>
2024-07-11 00:21:00 +0200

    The 'create invoice' functionality has been successfully completed.

--- src/app/modules/invoice/components/invoice/invoice.component.ts
+++ src/app/modules/invoice/components/invoice/invoice.component.ts
@@ -1,10 +1,11 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { MarketService } from '../../../market/shared/services/market.service';
-import { BehaviorSubject, Observable, switchMap } from 'rxjs';
+import { BehaviorSubject, Observable, Subscription, switchMap } from 'rxjs';
 import { Market } from '../../../market/models/market.interface';
 import { Shop } from '../../../shop/models/shop.interface';
 import { ShopService } from '../../../shop/shared/services/shop.service';
 import { ToastService } from '../../../shared-components/services/toast.service';
+import { InvoiceService } from '../../shared/services/invoice.service';
 
 @Component({
   selector: 'app-invoice',
@@ -12,21 +13,26 @@ import { ToastService } from '../../../shared-components/services/toast.service'
   styleUrl: './invoice.component.css'
 })
 
-export class InvoiceComponent implements OnInit {
+export class InvoiceComponent implements OnInit, OnDestroy {
 
+  private refreshShops$ = new BehaviorSubject<void>(undefined);
+
+  private invoiceService = inject(InvoiceService);
   protected marketService = inject(MarketService);
   private shopService = inject(ShopService);
   private toastService = inject(ToastService);
 
   marketList$!: Observable<Market[]>;
   shopList$!: Observable<Shop[]>;
-  private refreshShops$ = new BehaviorSubject<void>(undefined);
-  total!: number;
-  textBtn: string = 'Valider ma Facture';
+
+  postSubscribtion$: Subscription = new Subscription;
+
   marketContent: string = "market";
   activeMarket?: number;
   shopContent: string = "shop";
   activeShop?: number;
+  total!: number;
+  textBtn: string = 'Valider ma Facture';
 
   ngOnInit(): void {
     this.marketList$ = this.marketService.getAllMarket$();
@@ -53,5 +59,26 @@ export class InvoiceComponent implements OnInit {
       },
       error: (error) => this.toastService.show("Une erreur s'est produite lors de la suppression", 'Erreur', 'error')
     });
+  }
+
+  onSubmit(): void {
+    if (this.total === undefined || this.activeMarket === undefined || this.activeShop === undefined) {
+      console.log("total: " + this.total + ", market_id: " + this.activeMarket + ", shop_id: " + this.activeShop);
+      this.toastService.show('Tous les champs ne sont pas remplis.', 'Erreur', 'error');
+    } else {
+      console.log("total: " + this.total + " market_id: " + this.activeMarket + " shop_id: " + this.activeShop);
+     this.postSubscribtion$ = this.invoiceService.postInvoice$(this.total, this.activeMarket, this.activeShop).subscribe({
+        next: (response) => {
+          this.toastService.show('Facture validée avec succès.', 'Succès', 'success');
+        },
+        error: (error) => {
+          this.toastService.show('La facture est déjà existante.', 'Erreur', 'error');
+        }
+      });
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.postSubscribtion$.unsubscribe();
   }
 }
