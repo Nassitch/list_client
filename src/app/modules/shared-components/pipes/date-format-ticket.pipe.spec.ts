5221a245c3a07823a52ecd013a33bd7635e27353
Nassime HARMACH <Nassitch@hotmail.fr>
2024-08-01 00:35:50 +0100

    Config of invoice display per year and month.

---
+++ src/app/modules/shared-components/pipes/date-format-ticket.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { DateFormatTicketPipe } from './date-format-ticket.pipe';
+
+describe('DateFormatTicketPipe', () => {
+  it('create an instance', () => {
+    const pipe = new DateFormatTicketPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
