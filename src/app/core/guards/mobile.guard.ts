5203832dcde45ec75a412255ca2784a86e23026d
Nassitch <Nassitch@hotmail.fr>
2024-08-08 14:18:18 +0200

    Update to version 1.02.

--- src/app/core/guards/mobile.guard.ts
+++ src/app/core/guards/mobile.guard.ts
@@ -1,14 +1,21 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { DeviceService } from '../../modules/shared-components/services/device.service';
+import { StorageService } from '../services/storage.service';
 
 export const mobileGuard: CanActivateFn = (route, state) => {
-  
   const deviceService = inject(DeviceService);
+  const storageService = inject(StorageService);
   const router = inject(Router);
 
   if (deviceService.isMobileDevice()) {
-    return true;
+    if (storageService.getItem('installed')) {
+      return true;
+    } else {
+      storageService.setItem('installed', false);
+      router.navigate(['/landing-page']);
+      return false;
+    }
   } else {
     router.navigate(['/landing-page']);
     return false;
